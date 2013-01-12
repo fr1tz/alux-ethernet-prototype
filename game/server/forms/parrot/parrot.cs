fc699591b7d1e113e2431edde24e250361c3938f
Michael Goldener <mg@wasted.ch>
2013-01-12 14:21:09 +0100

    Remove engine sound from Parrot.

--- game/server/forms/parrot/parrot.cs
+++ game/server/forms/parrot/parrot.cs
@@ -127,7 +127,7 @@ datablock FlyingVehicleData(FrmParrot)
 
    //
 //   jetSound = Team1ScoutScoutDroneThrustSound;
-   engineSound = FrmParrotEngineSound;
+//   engineSound = FrmParrotEngineSound;
 //   softImpactSound = SoftImpactSound;
 //   hardImpactSound = HardImpactSound;
    //wheelImpactSound = WheelImpactSound;
